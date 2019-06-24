733a3e25bdaa4e24a0daa7c70684c81773ac26e1
Rémi PONCHE <ponche62880@gmail.com>
2019-06-25 01:13:58 +0200

    output management

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import _ from 'lodash' ;
 import './style.css' ; 
 import Icon from './icon.png' ; 
 import Data from './data.xml' ; 
+import printMe from './print.js' ; 
 
 function component() {
 	const element = document.createElement('div') ; 
+	const btn = document.createElement('button'); 
 
 	element.innerHTML = _.join(['Hello', 'webpack'], ' '); 
 	element.classList.add('hello');
+
+	btn.innerHTML = 'Click me and check the console!' ; 
+	btn.onclick = printMe; 
+
+	element.appendChild(btn); 
+	
 	
 	// Add the image to our existing div 
 	const myIcon = new Image(); 
